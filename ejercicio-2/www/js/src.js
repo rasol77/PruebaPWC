252e6390b44de9dd06b1e48756f6ff5859e46e6e
Carlos <58401176+rasol77@users.noreply.github.com>
2022-04-10 19:19:11 +0200

    Add files via upload

--- ejercicio-2/www/js/src.js
+++ ejercicio-2/www/js/src.js
@@ -1,13 +1,17 @@
-'use strict'
+'use strict';
 /*
 Solicita a un usuario su nombre y su primer apellido. Posteriormente pregúntale 
 si le apetece un café.*/
 
 let nombre = prompt('¿Dime tu nombre');
 let apellido = prompt('¿Dime tus apellidos');
-let coffe   = prompt('¿Quieres un café');
+let coffe = confirm('¿Quieres un café');
 /*- Si el usuario acepta mostraremos un mensaje del tipo: "¡Bienvenid@ Juan Campos! 
 En breves recibirá su café calentito en casa."*/
-console.prompt (`¡Bienvenid@ ${nombre} ${apellido} en breves recibiras un café caliente en casa`);
-
-
+if (coffe === true) {
+  alert(
+    `¡Bienvenid@ ${nombre} ${apellido} en breves recibiras un café caliente en casa`
+  );
+} else {
+  alert(`¡Bienvenid@ ${nombre} ${apellido} NO Ay Café`);
+}
